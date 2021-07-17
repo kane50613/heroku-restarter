16e6200cbd2bcce5d7bb284ac2244be7eaa03484
凱恩Kane <50613gary@gmail.com>
2021-07-17 15:30:34 +0800

    remove support for 13.x or lower :(

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const Restarter = require("./base/Restarter")
 
 module.exports = (token, name) => {
-	if(!token || token.length === 0)
+	if(!token || token?.length === 0)
 		throw new Error(`Heroku API token is required! more info here https://devcenter.heroku.com/articles/authentication`)
-	if(!name || name.length === 0)
+	if(!name || name?.length === 0)
 		throw new Error(`App name is required`)
 	return new Restarter(token, name)
 }
