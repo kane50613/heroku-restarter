9ff454bbc7621491a327ef817b49e1b9bc6e9188
凱恩Kane <50613gary@gmail.com>
2021-07-17 15:26:26 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
 
-;(async () => {
-	console.log(await require("../src/index")('a', process.env.HEROKU_APP).list())
-})()
+require("../src/index")(process.env.HEROKU_API_KEY, process.env.HEROKU_APP).list()
+.then(console.log)
+.catch(console.log)
