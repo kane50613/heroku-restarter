5aedaecb6136aaffb272891972e628d4c76bb553
凱恩Kane <50613gary@gmail.com>
2021-07-17 15:28:52 +0800

    support for 12.x

--- test/index.js
+++ test/index.js
@@ -1,5 +1,9 @@
 require("dotenv").config()
 
-require("../src/index")(process.env.HEROKU_API_KEY, process.env.HEROKU_APP).list()
-.then(console.log)
-.catch(console.log)
+try {
+	require("../src/index")(process.env.HEROKU_API_KEY, process.env.HEROKU_APP).list()
+	.then(console.log)
+	.catch(console.log)
+} catch (e) {
+	console.log(e)
+}
